01d530c63f73bf1506c91ec7eedd7894ff927d17
Joe Quintero <joedarwin1998@gmail.com>
2022-06-26 14:45:55 -0400

    querySelector

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//Seleccionar Contenido
+
+//QuerySelector
+const heading = document.querySelector('.header__texto h2') //Retorna 0 o 1 elementos
+heading.textContent = 'Nuevo Heading'; //Cambiamos el nombre
+heading.classList.add('nueva-clase'); //Agregamos nueva clase a la etiqueta
+
+console.log(heading);
+
+//QuerySelectorAll
+
+
+
+//getElementById

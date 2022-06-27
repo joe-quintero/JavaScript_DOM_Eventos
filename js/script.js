01556e93768c1a142d3b75b0ce0736626c1035fe
Joe Quintero <joedarwin1998@gmail.com>
2022-06-26 23:47:04 -0400

    QuerySelectorAll 1/2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 //Seleccionar Contenido
 
-//QuerySelector
-const heading = document.querySelector('.header__texto h2') //Retorna 0 o 1 elementos
+//QuerySelector //Retorna 0 o 1 elementos
+const heading = document.querySelector('.header__texto h2')
 heading.textContent = 'Nuevo Heading'; //Cambiamos el nombre
 heading.classList.add('nueva-clase'); //Agregamos nueva clase a la etiqueta
 
 console.log(heading);
 
-//QuerySelectorAll
+//QuerySelectorAll //Retorna 0 hasta la cantidad de selectores que alla
 
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces[0]);
+
+enlaces[0].textContent = 'Nuevo Enlace'; //modificacmos en mombre del enlace
+enlaces[0].href = 'http://www.google.com'; //modificamos la direccion del enlace
 
 
 //getElementById

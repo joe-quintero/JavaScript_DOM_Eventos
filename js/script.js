b6d4df65ec9d755021f0bd7bf21669e561f58f83
Joe Quintero <joedarwin1998@gmail.com>
2022-06-27 19:20:19 -0400

    Crear HTML con createElement

--- js/script.js
+++ js/script.js
@@ -20,3 +20,23 @@ enlaces[0].classList.remove('nueva-clase'); // Eliminamos clase del enlace
 //getElementById
 const heading2 = document.getElementById('heading');
 console.log(heading2);
+
+/*Generar codigo HTML (Nuevo enlace)------------------------------------------------------*/
+
+//Creamos la etiqueta
+const nuevoEnlace = document.createElement('A');
+//Agregar href
+nuevoEnlace.href = ('nuevo-enlace.html');
+//Agregar texto
+nuevoEnlace.textContent = ('Un Nuevo Enlace');
+//agregar calse
+nuevoEnlace.classList.add('navegacion__enlace');
+
+//Agregar al documento HTML
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+/*Resultado : <a href="nuevo-enlace.html" class="navegacion__enlace">Un Nuevo Enlace<a>*/
+
+
+console.log(nuevoEnlace);
